3ef6804793ae99b2e4340dfbdeac5926fb15fbb0
Adam Cerny <adamcernycz@gmail.com>
2015-08-30 08:10:51 +0200

    Refactor of CoreFragment + minor changes

--- RxJavademo/app/src/main/java/com/strv/rxjavademo/fragment/CoreFragment.java
+++ RxJavademo/app/src/main/java/com/strv/rxjavademo/fragment/CoreFragment.java
@@ -3,6 +3,7 @@ package com.strv.rxjavademo.fragment;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -46,83 +47,54 @@ public class CoreFragment extends BaseFragment
 	@OnClick(R.id.fragment_core_click_button)
 	public void clickCounterDemo()
 	{
-		getActivity().getSupportFragmentManager()
-				.beginTransaction()
-				.addToBackStack(ClickCounterFragment.class.getName())
-				.setCustomAnimations(R.anim.slide_enter, R.anim.slide_exit, R.anim.slide_enter_pop, R.anim.slide_exit_pop)
-				.replace(android.R.id.content,
-						ClickCounterFragment.newInstance(),
-						ClickCounterFragment.class.getName())
-				.commit();
+		showFragment(ClickCounterFragment.class.getName(), ClickCounterFragment.newInstance());
 	}
 
 
 	@OnClick(R.id.fragment_core_edittext_button)
 	public void clickEditTextDemo()
 	{
-		getActivity().getSupportFragmentManager()
-				.beginTransaction()
-				.addToBackStack(EditTextFragment.class.getName())
-				.setCustomAnimations(R.anim.slide_enter, R.anim.slide_exit, R.anim.slide_enter_pop, R.anim.slide_exit_pop)
-				.replace(android.R.id.content,
-						EditTextFragment.newInstance(),
-						EditTextFragment.class.getName())
-				.commit();
+		showFragment(EditTextFragment.class.getName(), EditTextFragment.newInstance());
 	}
 
 
 	@OnClick(R.id.fragment_core_form_button)
 	public void clickFormDemo()
 	{
-		getActivity().getSupportFragmentManager()
-				.beginTransaction()
-				.addToBackStack(FormFragment.class.getName())
-				.setCustomAnimations(R.anim.slide_enter, R.anim.slide_exit, R.anim.slide_enter_pop, R.anim.slide_exit_pop)
-				.replace(android.R.id.content,
-						FormFragment.newInstance(),
-						FormFragment.class.getName())
-				.commit();
+		showFragment(FormFragment.class.getName(), FormFragment.newInstance());
 	}
 
 
 	@OnClick(R.id.fragment_core_autocomplete_button)
 	public void clickAutocompleteDemo()
 	{
-		getActivity().getSupportFragmentManager()
-				.beginTransaction()
-				.addToBackStack(AutocompleteFragment.class.getName())
-				.setCustomAnimations(R.anim.slide_enter, R.anim.slide_exit, R.anim.slide_enter_pop, R.anim.slide_exit_pop)
-				.replace(android.R.id.content,
-						AutocompleteFragment.newInstance(),
-						AutocompleteFragment.class.getName())
-				.commit();
+		showFragment(AutocompleteFragment.class.getName(), AutocompleteFragment.newInstance());
 	}
 
 
 	@OnClick(R.id.fragment_core_retrofit_button)
 	public void clickRetroFitDemo()
 	{
-		getActivity().getSupportFragmentManager()
-				.beginTransaction()
-				.addToBackStack(RetrofitFragment.class.getName())
-				.setCustomAnimations(R.anim.slide_enter, R.anim.slide_exit, R.anim.slide_enter_pop, R.anim.slide_exit_pop)
-				.replace(android.R.id.content,
-						RetrofitFragment.newInstance(),
-						RetrofitFragment.class.getName())
-				.commit();
+		showFragment(RetrofitFragment.class.getName(), RetrofitFragment.newInstance());
 	}
 
 
 	@OnClick(R.id.fragment_core_firebase_button)
 	public void clickFirebaseDemo()
 	{
+		showFragment(FirebaseFragment.class.getName(), FirebaseFragment.newInstance());
+	}
+
+
+	private void showFragment(String className, Fragment fragment)
+	{
 		getActivity().getSupportFragmentManager()
 				.beginTransaction()
-				.addToBackStack(FirebaseFragment.class.getName())
+				.addToBackStack(className)
 				.setCustomAnimations(R.anim.slide_enter, R.anim.slide_exit, R.anim.slide_enter_pop, R.anim.slide_exit_pop)
 				.replace(android.R.id.content,
-						FirebaseFragment.newInstance(),
-						FirebaseFragment.class.getName())
+						fragment,
+						className)
 				.commit();
 	}
 
